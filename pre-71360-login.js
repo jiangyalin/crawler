17da7b302c378e86d206a4a16d4050a45c7fb3ba
yalin.jiang <yalin.jiang@71360.com>
2019-10-29 09:35:05 +0800

    xx

--- pre-71360-login.js
+++ pre-71360-login.js
@@ -17,10 +17,13 @@ async function example() {
     const pwd = await driver.findElement(By.css('#app .login .login-container .el-form .el-form-item:nth-of-type(2) .el-input__inner')).sendKeys('zhendao123@123')
     // console.log('kkk', driver.findElement(By.css('#app')))
     // console.log('css', driver.findElement(By.css('#app .login .login-container .el-form .el-form-item:nth-of-type(3) button')).getId('class').then(res => console.log(res)))
-    const btn = await driver.findElement(By.css('#app .login .login-container .el-form .el-form-item:nth-of-type(3) button')).getText()
-    const js = 'console.log(\'aa\', document.getElementById(\'app\'))'
+    // const btn = await driver.findElement(By.css('#app .login .login-container .el-form .el-form-item:nth-of-type(3) button')).getText()
+    const btn = await driver.findElement(By.css('#app .login .login-container .el-form .el-form-item:nth-of-type(3) button')).getAttribute('type')
+    const js = 'const demo = document.getElementById(\'app\')\n' +
+      '    console.log(\'demo\', demo)\n' +
+      '    return demo'
 
-    const res = await driver.executeAsyncScript(js)
+    const res = await driver.executeScript(js)
 
     console.log('res', res)
 
